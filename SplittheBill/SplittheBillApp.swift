8a8c34b1657c50cec1b4ab230f41a2958f5c05f3
Imevbore <ketuimevbore@yahoo.com>
2024-01-18 20:10:29 +0000

    Initial Commit

---
+++ SplittheBill/SplittheBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplittheBillApp.swift
+//  SplittheBill
+//
+//  Created by Elena for AP on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SplittheBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
